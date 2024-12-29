905a08b8235eec0a42c53c51ed745f9a80496bd8
Lovro Sikošek <67958862+lsikosek@users.noreply.github.com>
2024-12-29 18:31:28 +0100

    Nastavil stripe dependency in popravil strukturo projekta, ki je bila pokvarjena zaradi uporabe templata

---
+++ storitve/src/main/java/org/ekipaenajst/beans/StripeZrno.java
@@ -0,0 +1,17 @@
+package org.ekipaenajst.beans;
+
+import com.stripe.model.Charge;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Currency;
+
+@ApplicationScoped
+public class StripeZrno {
+
+    private Currency currency;
+
+    public Charge getCharge() {
+        // TODO ....
+        return null;
+    }
+}
